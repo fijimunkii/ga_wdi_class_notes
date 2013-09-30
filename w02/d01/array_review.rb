6f9cb68bb6b710d780f655c2d41acbbc2356bdda
Harrison Powers <fijimunkii@gmail.com>
2013-09-30 09:14:39 -0400

    morning array review

---
+++ w02/d01/array_review.rb
@@ -0,0 +1,21 @@
+names = ["Alex", "Jon", "Harry"]
+
+names << "Morgan"
+names << "Travis"
+
+names.push("Quin")
+names.push("Tasha")
+
+names.unshift("Diego")
+names.unshift("Ana")
+
+names.shift
+names.pop
+
+names.count
+
+names.each do |x| print x end
+
+names.shuffle
+
+names.include?)("Sally")
