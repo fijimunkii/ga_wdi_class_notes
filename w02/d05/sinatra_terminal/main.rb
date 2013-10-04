9ce6682a60b73a81953174a7f975b2ef0544a981
Harrison Powers <fijimunkii@gmail.com>
2013-10-04 16:01:54 -0400

    shows real terminal output!

--- w02/d05/sinatra_terminal/main.rb
+++ w02/d05/sinatra_terminal/main.rb
@@ -4,8 +4,9 @@ require 'sinatra/reloader' if development?
 
 
 get '/' do
-	`#{params[:terminal_input]}` if params[:terminal_input]
 	@previous_lines ||= ''
 	@previous_lines += "#{params[:terminal_input]}<br />" if params[:terminal_input]
+	@previous_lines += `#{params[:terminal_input]}` + '<br />' if params[:terminal_input]
+	
 	erb :terminal
 end
