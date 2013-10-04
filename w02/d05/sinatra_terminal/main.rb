94062fb5552120790d37aab4198b4bfd76c2dcdb
Harrison Powers <fijimunkii@gmail.com>
2013-10-04 16:35:44 -0400

    post instead of get

--- w02/d05/sinatra_terminal/main.rb
+++ w02/d05/sinatra_terminal/main.rb
@@ -1,12 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-
+previous_lines = ''
 
 get '/' do
-	@previous_lines ||= ''
-	@previous_lines += "#{params[:terminal_input]}<br />" if params[:terminal_input]
-	@previous_lines += `#{params[:terminal_input]}` + '<br />' if params[:terminal_input]
-	
+	previous_lines += "<br />#{params[:terminal_input]}" if params[:terminal_input]
+	previous_lines += '<br />' + `#{params[:terminal_input]}` if params[:terminal_input]
+	@previous_lines = previous_lines
+	erb :terminal
+end
+
+post '/' do
+	previous_lines += "<br />#{params[:terminal_input]}" if params[:terminal_input]
+	previous_lines += '<br />' + `#{params[:terminal_input]}` if params[:terminal_input]
+	@previous_lines = previous_lines
 	erb :terminal
 end
