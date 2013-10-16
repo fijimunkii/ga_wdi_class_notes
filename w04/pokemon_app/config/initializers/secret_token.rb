495b8d3d7059a1bdf479d0928e1d05e631086aef
Harrison Powers <fijimunkii@gmail.com>
2013-10-16 15:47:19 -0400

    create new pokemon rails app

---
+++ w04/pokemon_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokemonApp::Application.config.secret_token = 'aa02302e4049adc7ece0168d9f7b22bbf6288a6897de5ce4a5dc0eba7d3c535b2852b64ec61fd50dab8cf2a333a2adbe943549da1123f13a40d2f8ded0180bfa'
