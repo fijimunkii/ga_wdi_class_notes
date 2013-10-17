68dd4a0abb1317a37a835ca161974bc63b393d06
Harrison Powers <fijimunkii@gmail.com>
2013-10-17 12:05:11 -0400

    hogwarts controllers

--- w04/d03/hogwarts/db/schema.rb
+++ w04/d03/hogwarts/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131017045809) do
+ActiveRecord::Schema.define(:version => 20131017155213) do
 
   create_table "houses", :force => true do |t|
-    t.integer  "name"
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
