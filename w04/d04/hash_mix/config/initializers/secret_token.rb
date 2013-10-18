46caaa2d9159fbcee6ec6e06c68397be677dc433
Harrison Powers <fijimunkii@gmail.com>
2013-10-18 10:07:30 -0400

    new rails app hash_mix

---
+++ w04/d04/hash_mix/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HashMix::Application.config.secret_token = 'eea98232ce9ae943033b4dd9f38d63dff5657825039ef9b19198e092a6dd35926b65e1e48a04c19690d7a99a3b569cfd5194650f49c6c4453ec71f5365acdcc7'
