558898531f03a6f30fdb17e27d13308d23420d14
Harrison Powers <fijimunkii@gmail.com>
2013-11-05 15:54:42 -0500

    time for pokemon

---
+++ w07/d02/pokemon_js/js/paint.js
@@ -0,0 +1,21 @@
+var Paint = Paint || {};
+
+Paint.loadTiles = function(rows, num, size) {
+  var $table = $('<table>');
+  $('body').append($table);
+
+  var tableString = '';
+
+  for (var r=0; r<rows; r++){
+    tableString += '<tr>';
+
+    for (var i=0; i<num; i++) {
+      tableString += '<td><div class=\'tile painted-white\' style=\'height:' + size + 'px; width: ' + size + 'px;\'></div></td>';
+    }
+
+    tableString += '</tr>';
+  }
+  $table.html(tableString);
+}
+
+Paint.curColor = 'green';
