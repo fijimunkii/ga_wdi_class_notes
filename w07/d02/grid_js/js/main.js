c45e7d274f4949e76c46d6612e67e3392d67f72b
Harrison Powers <fijimunkii@gmail.com>
2013-11-05 15:41:58 -0500

    custom colors

--- w07/d02/grid_js/js/main.js
+++ w07/d02/grid_js/js/main.js
@@ -1,39 +1,58 @@
 (function() {
-
-  var $header = $('<header>'),
-      $button = $('<button>');
+  var $header = $('<header>');
   $('body').append($header);
-  $header.append($button);
 
-  $button.on('click', function(){
-    var colors = ['white','red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-    var curIndex = colors.indexOf($('body')[0].className);
+  var colors = ['white','red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  for (var i=0; i<colors.length; i++) {
 
-    if (curIndex === colors.length-1) {
-      $('body').removeClass(colors[curIndex]);
-      $('body').addClass(colors[0]);
-    } else {
-      $('body').removeClass(colors[curIndex]);
-      $('body').addClass(colors[curIndex+1]);
-    }
+    $button = $('<button>');
+    $button.html(colors[i]);
 
+    $header.append($button);
+
+    $button.on('click', function(i){
+      return function() {
+        Paint.curColor = colors[i];
+        var classToRemove = $header.attr('class');
+        $header.removeClass(classToRemove);
+        $header.addClass('painted-' + colors[i]);
+      }
+    }(i));
+  }
+
+  $newColorInput = $('<input>');
+  $newColorSubmit = $('<div>');
+  $newColorSubmit.addClass('color-submit');
+  $newColorSubmit.html('Submit!')
+
+  $header.append($newColorInput);
+  $header.append($newColorSubmit);
+
+  $newColorSubmit.on('click', function() {
+    var newColor = $newColorInput[0].value,
+        style = $('<style>.painted-' +newColor + ', .hover-' + newColor +' { background: ' + newColor + '; }</style>');
+    $('html > head').append(style);
+    Paint.curColor = newColor;
+    var classToRemove = $header.attr('class');
+    $header.removeClass(classToRemove);
+    $header.addClass('painted-' + newColor);
   });
 
-  Paint.loadTiles(10, 10, 50);
+  Paint.loadTiles(60, 80, 10);
 
   $('.tile').on('mouseover', function(e) {
-    $(this).addClass('hover-green');
+    $(this).addClass('hover-' + Paint.curColor);
     if (e.which === 1) {
-      $(this).addClass('painted-green');
+      $(this).addClass('painted-' + Paint.curColor);
     }
   });
 
   $('.tile').on('mouseout', function() {
-    $(this).removeClass('hover-green');
+    $(this).removeClass('hover-' + Paint.curColor);
   });
 
   $('.tile').on('mousedown', function() {
-    $(this).addClass('painted-green');
+    $(this).addClass('painted-' + Paint.curColor);
   })
 
 })();
--- w07/d02/grid_js/js/paint.js
+++ w07/d02/grid_js/js/paint.js
@@ -4,21 +4,18 @@ Paint.loadTiles = function(rows, num, size) {
   var $table = $('<table>');
   $('body').append($table);
 
+  var tableString = '';
+
   for (var r=0; r<rows; r++){
-    var $tr = $('<tr>');
-    $table.append($tr);
+    tableString += '<tr>';
 
     for (var i=0; i<num; i++) {
-      var $td = $('<td>');
-          $tile = $('<div>');
-      $tile[0].id = 'tile-'+i;
-      $tile.addClass('tile');
-      $tile.addClass('white');
-      $tile.css('height', size);
-      $tile.css('width', size);
-
-      $td.append($tile);
-      $tr.append($td)
+      tableString += '<td><div class=\'tile white\' style=\'height:' + size + 'px; width: ' + size + 'px;\'></div></td>';
     }
+
+    tableString += '</tr>';
   }
+  $table.html(tableString);
 }
+
+Paint.curColor = 'green';
