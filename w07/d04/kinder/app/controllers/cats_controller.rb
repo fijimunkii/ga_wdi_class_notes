4c754762fd6edc603413f3760b9ab8f2982aca4f
Harrison Powers <fijimunkii@gmail.com>
2013-11-07 10:41:06 -0500

    welcome controller + index view

--- w07/d04/kinder/app/controllers/cats_controller.rb
+++ w07/d04/kinder/app/controllers/cats_controller.rb
@@ -3,12 +3,17 @@ class CatsController < ApplicationController
   respond_to :json
 
   def index
-    @cats = Cat.all
-    respond_with @cats
+    cats = Cat.all
+    @cat = cats.sample
+    respond_with @cat
   end
 
   def create
     @cat = Cat.create params[cat]
+  end
+
+  def update
+    @cat = Cat.find params[id]
   end
 
 end
