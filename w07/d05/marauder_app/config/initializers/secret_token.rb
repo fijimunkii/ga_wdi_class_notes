908317a19efb4ff1494fa41b70372353f1374716
Harrison Powers <fijimunkii@gmail.com>
2013-11-08 09:30:01 -0500

    new marauder app

---
+++ w07/d05/marauder_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarauderApp::Application.config.secret_token = 'ed277477baf329d5a08a572a67c366971d468a5af2303f1a48e15136a925eb694689eef5401f2013654dea634a3b7fd6540cdae9efc752daa75ab8dc07fc1eb7'
