f590b707ec6d0fd391ae7d0c2d7846a99201e9b5
Harrison Powers <fijimunkii@gmail.com>
2013-11-18 12:30:40 -0500

    adds and removes passengers

--- w09/d01/rspec/bus.rb
+++ w09/d01/rspec/bus.rb
@@ -5,7 +5,25 @@ class Bus
   end
 
   def empty?
-    @passengers.count == 0
+    self.size == 0
+  end
+
+  def contains?(passenger)
+    @passengers.include?(passenger)
+  end
+
+  def add(passenger)
+    if !self.contains?(passenger)
+      @passengers << passenger
+    end
+  end
+
+  def remove(passenger)
+    @passengers.delete(passenger)
+  end
+
+  def size
+    @passengers.count
   end
 
 
--- w09/d01/rspec/spec/bus_spec.rb
+++ w09/d01/rspec/spec/bus_spec.rb
@@ -7,5 +7,40 @@ describe Bus do
     expect(Bus.new).to be_empty
   end
 
+  it "can add a passenger" do
+    bus = Bus.new
+    bus.add("Mick Jagger")
+    expect(bus).not_to be_empty
+  end
+
+  it "can take more than one passenger" do
+    bus = Bus.new
+    bus.add("Mick Jagger")
+    bus.add("Keith Richards")
+    expect(bus.size).to eq(2)
+    bus.add("Charlie Watts")
+    expect(bus.size).to eq(3)
+  end
+
+  it "can remove a passenger" do
+    bus = Bus.new
+    bus.add("Mick Jagger")
+    bus.remove("Mick Jagger")
+    expect(bus).to be_empty
+  end
+
+  it "can determine if it has a specific rider" do
+    bus = Bus.new
+    expect(bus.contains?("Mick Jagger")).to be_false
+    bus.add("Mick Jagger")
+    expect(bus.contains?("Mick Jagger")).to be_true
+  end
+
+  it "wont let you add the same passenger twice" do
+    bus = Bus.new
+    bus.add("Mick Jagger")
+    bus.add("Mick Jagger")
+    expect(bus.size).to eq(1)
+  end
 
 end
