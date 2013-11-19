1f2e534ad7e834242c49390f83b21639d7881804
Harrison Powers <fijimunkii@gmail.com>
2013-11-19 13:46:36 -0500

    new rails proj

---
+++ w09/d02/afternoon-rails/pokemon/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokemon::Application.config.secret_token = 'bc5ee579fb668996291f49b8b5fc318de9f1124c201a40804316e6cea26a169ac601d8b79b8a4109826abf90bae28508917c5c6049c0d05ce35fdb9322327bb8'
